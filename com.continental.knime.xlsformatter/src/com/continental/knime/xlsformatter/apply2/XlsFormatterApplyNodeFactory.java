bfb4767d74a34654322c611a22aa01ad3d579e6e
Magnus Gohm <magnus.gohm@knime.com>
2026-01-02 16:01:44 +0100

    UIEXT-3159: WebUI Migration of XLS Formatter (apply) node
    UIEXT-3159 (WebUI-Migration XLS Formatter (apply))

--- com.continental.knime.xlsformatter/src/com/continental/knime/xlsformatter/apply2/XlsFormatterApplyNodeFactory.java
+++ com.continental.knime.xlsformatter/src/com/continental/knime/xlsformatter/apply2/XlsFormatterApplyNodeFactory.java
@@ -28,8 +28,25 @@ import org.knime.core.node.context.ports.PortsConfiguration;
 import org.knime.filehandling.core.port.FileSystemPortObject;
 
 import com.continental.knime.xlsformatter.porttype.XlsFormatterState;
+import org.knime.core.webui.node.dialog.NodeDialog;
+import org.knime.core.webui.node.dialog.NodeDialogFactory;
+import org.knime.core.webui.node.dialog.NodeDialogManager;
+import org.knime.core.webui.node.dialog.SettingsType;
+import org.knime.core.webui.node.dialog.defaultdialog.DefaultNodeDialog;
+import org.knime.core.webui.node.dialog.defaultdialog.DefaultKaiNodeInterface;
+import org.knime.core.webui.node.dialog.kai.KaiNodeInterface;
+import org.knime.core.webui.node.dialog.kai.KaiNodeInterfaceFactory;
+import org.knime.core.node.NodeDescription;
+import org.knime.node.impl.description.DefaultNodeDescriptionUtil;
+import java.util.Map;
+import org.knime.node.impl.description.PortDescription;
+import java.util.List;
+import static org.knime.node.impl.description.PortDescription.fixedPort;
+import static org.knime.node.impl.description.PortDescription.dynamicPort;
 
-public final class XlsFormatterApplyNodeFactory extends ConfigurableNodeFactory<XlsFormatterApplyNodeModel> {
+@SuppressWarnings("restriction")
+public final class XlsFormatterApplyNodeFactory extends ConfigurableNodeFactory<XlsFormatterApplyNodeModel> 
+	implements NodeDialogFactory, KaiNodeInterfaceFactory {
 
 	/** The name of the optional source connection input port group. */
 	private static final String CONNECTION_SOURCE_PORT_GRP_NAME = "Source File System Connection";
@@ -55,11 +72,6 @@ public final class XlsFormatterApplyNodeFactory extends ConfigurableNodeFactory<
 		return new XlsFormatterApplyNodeModel(portsCfg, createSettings(portsCfg), getFormatterIdx(portsCfg));
 	}
 
-	@Override
-	protected NodeDialogPane createNodeDialogPane(NodeCreationConfiguration creationConfig) {
-		final PortsConfiguration portsCfg = getPortsCfg(creationConfig);
-		return new XlsFormatterApplyNodeDialog(createSettings(portsCfg));
-	}
 
 	@Override
 	protected int getNrNodeViews() {
@@ -88,4 +100,84 @@ public final class XlsFormatterApplyNodeFactory extends ConfigurableNodeFactory<
 	private static int getFormatterIdx(final PortsConfiguration portsCfg) {
 		return portsCfg.getInputPortLocation().get(FORMATTER_STATE_GRP_NAME)[0];
 	}
+	
+    private static final String NODE_NAME = "XLS Formatter (apply)";
+    
+    private static final String NODE_ICON = "../apply/apply.png";
+    
+    private static final String SHORT_DESCRIPTION = """
+            The XLS Formatter (apply) node applies the chained commands from XLS Formatter nodes to an unformatted
+                xlsx file.
+            """;
+    
+    private static final String FULL_DESCRIPTION = """
+            <p> The XLS Formatter (apply) node applies the chained commands from XLS Formatter nodes to an
+                unformatted xlsx file. </p> <p> All previous XLS Formatter nodes do not modify any xlsx files yet but
+                prepare the formatting steps to be taken here. The input file may not contain any formatting yet (except
+                very few styles that are tolerated due to date/time values handling). </p> <p><i>This node supports the
+                </i><a
+                href="https://docs.knime.com/latest/analytics_platform_file_handling_guide/index.html#path"><i>path flow
+                variable</i></a> <i> to specify the source and destination location. To convert the created path column
+                to a string column which is required by some nodes that have not been migrated yet you can use the </i>
+                <a href="https://kni.me/n/ZLCisQlHCzW4IiZG"><i>Path to String</i></a><i> node. For further information
+                about file handling in general see the </i><a
+                href="https://docs.knime.com/latest/analytics_platform_file_handling_guide/index.html"><i>File Handling
+                Guide</i></a>.<br /></p>
+            """;
+    
+    private static final List<PortDescription> INPUT_PORTS = List.of(
+            dynamicPort("Source File System Connection", "Source file system connection", """
+                The source file system connection.
+                """),
+            fixedPort("XLS Formatter", """
+                The XLS Formatter input port holding the collected formatting instructions for your xlsx file.
+                """),
+            dynamicPort("Destination File System Connection", "Destination file system connection", """
+                The destination file system connection.
+                """)
+    );
+    
+    private static final List<PortDescription> OUTPUT_PORTS = List.of();
+
+    /**
+     * {@inheritDoc}
+     * @since 1.7
+     */
+    @Override
+    public NodeDialogPane createNodeDialogPane(NodeCreationConfiguration creationConfig) {
+        return NodeDialogManager.createLegacyFlowVariableNodeDialog(createNodeDialog());
+    }
+
+    @Override
+    public NodeDialog createNodeDialog() {
+        return new DefaultNodeDialog(SettingsType.MODEL, XlsFormatterApplyNodeParameters.class);
+    }
+
+    @Override
+    public NodeDescription createNodeDescription() {
+        return DefaultNodeDescriptionUtil.createNodeDescription( //
+            NODE_NAME, //
+            NODE_ICON, //
+            INPUT_PORTS, //
+            OUTPUT_PORTS, //
+            SHORT_DESCRIPTION, //
+            FULL_DESCRIPTION, //
+            List.of(), //
+            XlsFormatterApplyNodeParameters.class, //
+            null, //
+            NodeType.Sink, //
+            List.of(), //
+            null //
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     * @since 1.7
+     */
+    @Override
+    public KaiNodeInterface createKaiNodeInterface() {
+        return new DefaultKaiNodeInterface(Map.of(SettingsType.MODEL, XlsFormatterApplyNodeParameters.class));
+    }
+    
 }
---
+++ com.continental.knime.xlsformatter/src/com/continental/knime/xlsformatter/apply2/XlsFormatterApplyNodeParameters.java
@@ -0,0 +1,249 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ------------------------------------------------------------------------
+ */
+    
+package com.continental.knime.xlsformatter.apply2;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+import org.knime.core.webui.node.dialog.defaultdialog.internal.file.FileReaderWidget;
+import org.knime.core.webui.node.dialog.defaultdialog.internal.file.FileSelectionWidget;
+import org.knime.core.webui.node.dialog.defaultdialog.internal.file.FileSystemOption;
+import org.knime.core.webui.node.dialog.defaultdialog.internal.file.FileWriterWidget;
+import org.knime.core.webui.node.dialog.defaultdialog.internal.file.SingleFileSelectionMode;
+import org.knime.core.webui.node.dialog.defaultdialog.internal.file.WithFileSystem;
+import org.knime.core.webui.node.dialog.defaultdialog.persistence.booleanhelpers.DoNotPersistBoolean;
+import org.knime.core.webui.node.dialog.defaultdialog.util.updates.StateComputationFailureException;
+import org.knime.core.webui.node.dialog.defaultdialog.widget.Modification;
+import org.knime.core.webui.node.dialog.defaultdialog.widget.Modification.WidgetGroupModifier;
+import org.knime.node.parameters.Advanced;
+import org.knime.node.parameters.NodeParameters;
+import org.knime.node.parameters.NodeParametersInput;
+import org.knime.node.parameters.Widget;
+import org.knime.node.parameters.layout.After;
+import org.knime.node.parameters.layout.Layout;
+import org.knime.node.parameters.layout.Section;
+import org.knime.node.parameters.migration.DefaultProvider;
+import org.knime.node.parameters.migration.LoadDefaultsForAbsentFields;
+import org.knime.node.parameters.migration.Migration;
+import org.knime.node.parameters.persistence.Persist;
+import org.knime.node.parameters.persistence.Persistor;
+import org.knime.node.parameters.persistence.legacy.LegacyFileWriter;
+import org.knime.node.parameters.persistence.legacy.LegacyFileWriterWithOverwritePolicyOptions;
+import org.knime.node.parameters.persistence.legacy.LegacyFileWriterWithOverwritePolicyOptions.OverwritePolicy;
+import org.knime.node.parameters.persistence.legacy.LegacyFileWriterWithOverwritePolicyOptions.OverwritePolicyChoicesProvider;
+import org.knime.node.parameters.updates.Effect;
+import org.knime.node.parameters.updates.Effect.EffectType;
+import org.knime.node.parameters.updates.EffectPredicate;
+import org.knime.node.parameters.updates.EffectPredicateProvider;
+import org.knime.node.parameters.updates.ParameterReference;
+import org.knime.node.parameters.updates.StateProvider;
+import org.knime.node.parameters.updates.ValueProvider;
+import org.knime.node.parameters.updates.ValueReference;
+import org.knime.node.parameters.updates.util.BooleanReference;
+
+/**
+ * Node parameters for XLS Formatter (apply).
+ * 
+ * @author Magnus Gohm, KNIME GmbH, Konstanz, Germany
+ * @author AI Migration Pipeline v1.2
+ */
+@SuppressWarnings("restriction")
+@LoadDefaultsForAbsentFields
+final class XlsFormatterApplyNodeParameters implements NodeParameters {
+
+	@Section(title = "Source")
+	interface SourceSection {
+	}
+
+	@Section(title = "Destination")
+	@After(SourceSection.class)
+	interface DestinationSection {
+	}
+	
+	@After(DestinationSection.class)
+	@Advanced
+	interface AdvancedSection {
+	}
+
+	@Layout(SourceSection.class)
+	@Widget(title = "Source file", description = """
+        Select a file system which stores the .xlsx file you want to read and specify the path to the file.
+        The file must be a valid Excel (.xlsx) file that will be formatted according to the formatting instructions
+        from the connected XLS Formatter port.
+        """)
+	@Modification(LegacyFileWriterModifier.class)
+    @FileReaderWidget(fileExtensions = {"xlsx"})
+    @Persist(configKey = "InputFile")
+    LegacyFileWriter m_sourceFile = new LegacyFileWriter();
+
+    static final class LegacyFileWriterModifier implements LegacyFileWriter.Modifier {
+        
+    	@Override
+        public void modify(final Modification.WidgetGroupModifier group) {
+            final var fileSelection = findFileSelection(group);
+            fileSelection //
+                .modifyAnnotation(Widget.class) //
+                .withProperty("title", "File") //
+                .withProperty("description", "Select a .xlsx file to apply the formatting to.") //
+                .modify();
+            fileSelection //
+                .addAnnotation(FileSelectionWidget.class) //
+                .withProperty("value", SingleFileSelectionMode.FILE) //
+                .modify();
+            fileSelection //
+            	.addAnnotation(WithFileSystem.class) //
+            	.withProperty("value", new FileSystemOption[]{FileSystemOption.LOCAL, FileSystemOption.SPACE,
+            			FileSystemOption.EMBEDDED, FileSystemOption.CONNECTED})
+            	.modify();
+        }
+    	
+    }
+	
+    @Layout(DestinationSection.class)
+    @Widget(title = "Destination file", description = """
+        Select a file system in which you want to write the formatted .xlsx file and specify the output path.
+        The formatted Excel file will be written to this location.
+        """)
+    @Modification(OutputFileModification.class)
+    @FileWriterWidget(fileExtension = "xlsx")
+    @Persist(configKey = "OutputFile")
+    @ValueReference(DestinationFileRef.class)
+    LegacyFileWriterWithOverwritePolicyOptions m_destinationFile = new LegacyFileWriterWithOverwritePolicyOptions();
+
+    static final class DestinationFileRef implements ParameterReference<LegacyFileWriterWithOverwritePolicyOptions> {
+    }
+    
+    static class OutputFileModification implements LegacyFileWriterWithOverwritePolicyOptions.Modifier {
+
+        @Override
+        public void modify(final WidgetGroupModifier group) {
+            findFileSelection(group) //
+                .modifyAnnotation(Widget.class) //
+                .withProperty("title", "File") //
+                .withProperty("description", "The file to write the formatted .xlsx to.") //
+                .modify();
+            findFileSelection(group) //
+                .addAnnotation(FileSelectionWidget.class) //
+                .withValue(SingleFileSelectionMode.FILE) //
+                .modify();
+            findFileSelection(group) //
+                .addAnnotation(WithFileSystem.class) //
+                .withProperty("value", new FileSystemOption[]{FileSystemOption.LOCAL, FileSystemOption.SPACE,
+                    FileSystemOption.EMBEDDED, FileSystemOption.CUSTOM_URL, FileSystemOption.CONNECTED})
+                .modify();
+            restrictOverwritePolicyOptions(group, XlsFormatterApplyOverwritePolicyChoicesProvider.class);
+        }
+        
+        static final class XlsFormatterApplyOverwritePolicyChoicesProvider extends OverwritePolicyChoicesProvider {
+
+			@Override
+			protected List<OverwritePolicy> getChoices() {
+				return List.of(OverwritePolicy.overwrite, OverwritePolicy.fail);
+			}
+			
+		}
+
+    }
+    
+    @Layout(AdvancedSection.class)
+    @Widget(title = "Preserve source file's cell number formats", description = """
+        When selected, the original number formats from the source file (e.g. date cells written by KNIME)
+        will be preserved in the output file. When unchecked, default formatting may be applied.
+        """)
+    @Persist(configKey = "PreserveSourceNumberFormats")
+    @Migration(LoadFalseForOldNodes.class)
+    boolean m_preserveSourceNumberFormats = true;
+    
+    static class LoadFalseForOldNodes implements DefaultProvider<Boolean> {
+    	
+        @Override
+        public Boolean getDefault() {
+            return false;
+        }
+        
+    }
+
+    @Layout(AdvancedSection.class)
+    @Widget(title = "Open output file after execution", description = """
+        Once the node execution has been finished, the output .xlsx file will be opened automatically.
+        Note: Only files on your local file system can be opened.
+        """)
+    @Persist(configKey = "OpenOutputFile")
+    @Effect(predicate = IsOpenOutputFileEnabled.class, type = EffectType.ENABLE)
+    boolean m_openOutputFile;
+    
+    @Persistor(DoNotPersistBoolean.class)
+    @ValueProvider(IsOpenOutputFileEnabledProvider.class)
+    @ValueReference(IsOpenOutputFileEnabled.class)
+    boolean m_isOpenOutputFileEnabled;
+    
+    static final class IsOpenOutputFileEnabled implements BooleanReference {
+    }
+    
+    static final class IsOpenOutputFileEnabledProvider implements StateProvider<Boolean> {
+
+    	private Supplier<LegacyFileWriterWithOverwritePolicyOptions> m_destinationFileSupplier;
+    	
+		@Override
+		public void init(StateProviderInitializer initializer) {
+			initializer.computeBeforeOpenDialog();
+			m_destinationFileSupplier = initializer.computeFromValueSupplier(DestinationFileRef.class);
+		}
+
+		@Override
+		public Boolean computeState(NodeParametersInput parametersInput) throws StateComputationFailureException {
+			final var destinationFile = m_destinationFileSupplier.get();
+			if (destinationFile == null) {
+				return false;
+			}
+			return !XlsFormatterApplyNodeModel.isHeadlessOrRemote() && XlsFormatterApplyNodeModel.categoryIsSupported(
+					destinationFile.getFileSelection().getFSLocation().getFSCategory());
+		}
+    	
+    }
+    
+}
